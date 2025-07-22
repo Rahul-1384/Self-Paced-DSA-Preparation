65b3bf9668c170bcaa861d56f1524a6c14000dfe
Rahul Rajput <156775643+rahul1384r@users.noreply.github.com>
2025-07-22 20:58:33 +0530

    [Rahul] feat: Object Oriented Programming initial commit

---
+++ Week-02_OOPs/ClassAndObjects.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class ClassAndObjects {
+    public static void main(String[] args) {
+        Student[] students = new Student[5];
+        Student rahul;
+        
+    }
+}
+class Student{
+    int rno;
+    String name;
+    float marks;
+}
