df3dfccde92825051b9bc57f1fbc8c17d0b9a289
Rahul Rajput <156775643+rahul1384r@users.noreply.github.com>
2025-08-04 15:47:44 +0530

    [Rahul] Mod: modiefied few parts of code

--- Week-03_Generics&CollectionsFrame/Day-02_Collections/Set/HashSets.java
+++ Week-03_Generics&CollectionsFrame/Day-02_Collections/Set/HashSets.java
@@ -5,6 +5,12 @@ public class HashSets{
         HashSet<Integer> set = new HashSet<>();
         set.add(2);
         set.add(4);
+        set.add(6);
+        set.add(8);
+        set.add(10);
+        set.add(12);
         System.out.println(set);
+        System.out.println(set.size());
+        System.out.println(set.contains(80));
     }
 }
